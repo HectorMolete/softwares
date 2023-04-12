6aa4cd48ef536b3d6545dcbdc398b5a704381e7c
Molete Tiisetso <113854746+HectorMolete@users.noreply.github.com>
2023-04-13 00:20:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
+
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/APP-NAME'));
+app.use(express.static('./dist/grind-automations'));
+
 app.get('/*', (req, res) =>
- res.sendFile('index.html', {root: 'dist/ APP-NAME /'}),
+    res.sendFile('index.html', {root: 'dist/grind-automations/'}),
 );
+const corsOptions ={
+    origin:'http://localhost:4200', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
