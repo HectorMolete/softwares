070215147dcac6a14c84cb5b7b62f2e7aaee4478
Molete Tiisetso <113854746+HectorMolete@users.noreply.github.com>
2023-04-13 01:16:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sonic-softwares'));
+app.use(express.static('./dist/sonicsoftwares'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/sonic-softwares/'}),
+    res.sendFile('index.html', {root: 'dist/sonicsoftwares/'}),
 );
-const corsOptions ={
-    origin:'http://localhost:4200', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
