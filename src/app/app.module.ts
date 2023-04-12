8c258196231cae887eedd0b87f2438828a08e73f
Molete Tiisetso <113854746+HectorMolete@users.noreply.github.com>
2023-04-12 23:35:06 +0200

    sonicSoftwares

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegistrationComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
